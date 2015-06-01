8befd55170c652eed039db8b114883d438d9dc1e
Jérémy Hagneré <jeremy.hagnere@gmail.com>
2015-06-01 15:27:12 -0400

    Setting up bootstrap with default configuration.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new \FOS\UserBundle\FOSUserBundle(),
             new Self\FrontBundle\SelfFrontBundle(),
             new Self\UserBundle\SelfUserBundle(),
+            new \Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
